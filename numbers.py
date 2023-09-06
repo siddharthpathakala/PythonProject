2b760ba76c1f684393297c3424671c54d4ff8cc4
Siddhu <siddharthpathakala@gmail.com>
2023-09-06 18:50:00 -0400

    Defect fixed in the print statement

--- numbers.py
+++ numbers.py
@@ -1,3 +1,3 @@
-print("***** This program prints the range of Numbers from 1 to 25" *****)
+print("***** This program prints the range of Numbers from 1 to 25 *****")
 for i in range(26):
 	print(i)
